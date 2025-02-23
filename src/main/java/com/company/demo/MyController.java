55d480542df47fb5e8060264792421a5348deb93
Vishal Singh <vishal267s@gmail.com>
2025-02-23 15:26:12 +0530

    ok

--- src/main/java/com/company/demo/MyController.java
+++ src/main/java/com/company/demo/MyController.java
@@ -1,5 +1,6 @@
 package com.company.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,4 +13,9 @@ public class MyController {
 		
 		return "HEllo "+name;
 	}
+	
+	@GetMapping("/hello")
+    public String hello(){
+        return ("<h1>Welcome</h1>");
+    }
 }
